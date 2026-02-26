e0c700b665ffc276d6a49a58810f2c1974808e3e
pjoly <59795845+Pierre-Joly@users.noreply.github.com>
2026-02-26 15:49:14 +0100

    Initial Commit

---
+++ Fluxus/FluxusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FluxusApp.swift
+//  Fluxus
+//
+//  Created by Pierre joly on 26/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct FluxusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FluxusTests/FluxusTests.swift
@@ -0,0 +1,17 @@
+//
+//  FluxusTests.swift
+//  FluxusTests
+//
+//  Created by Pierre joly on 26/02/2026.
+//
+
+import Testing
+@testable import Fluxus
+
+struct FluxusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
